7658c039aa6fec7657e940a32b348bf940cb11f9
Bert Wijnants <bert.wijnants@gmail.com>
2012-05-27 18:07:12 +0200

    alert

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,16 +4,19 @@ document.addEventListener("deviceready", onDeviceReady, false);
 var watchID = null;
 
 function onDeviceReady() {
-    // Update every 3 seconds
-    var options = { frequency: 3000 };
+    // Update every 10 seconds
+    var options = { frequency: 10000 };
     watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
 
     document.getElementById("getLocation").onclick = function() {
-         navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        alert("click");
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }
 }
 
 function onSuccess(position) {
+    alert("position: " + position.coords.latitude + ", " + position.coords.longitude);
+
     var element = document.getElementById('geolocation');
     element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
                         'Longitude: '          + position.coords.longitude             + '<br />' +
