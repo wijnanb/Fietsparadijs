081f717554e8df1b4fa1ae43d7be7a3d7a84ee54
Bert Wijnants <bert.wijnants@gmail.com>
2012-05-27 17:47:01 +0200

    bump

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,13 +7,23 @@ function onDeviceReady() {
     // Update every 3 seconds
     var options = { frequency: 3000 };
     watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+
+    document.getElementById("getLocation").onclick = function() {
+         navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
 }
 
 function onSuccess(position) {
     var element = document.getElementById('geolocation');
-    element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
-                        'Longitude: ' + position.coords.longitude     + '<br />' +
-                        '<hr />'      + element.innerHTML;
+    element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                        'Longitude: '          + position.coords.longitude             + '<br />' +
+                        'Altitude: '           + position.coords.altitude              + '<br />' +
+                        'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                        'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                        'Heading: '            + position.coords.heading               + '<br />' +
+                        'Speed: '              + position.coords.speed                 + '<br />' +
+                        'Timestamp: '          + new Date(position.timestamp)          + '<br />';
+    }
 }
 
 function onError(error) {
